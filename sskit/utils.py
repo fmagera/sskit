6869603a9a61bc84bbf6d9cb2b2ed73d5a233bf5
Hakan Ardo <hakan.ardo@math.lth.se>
2024-08-14 15:56:29 +0200

    in progress

--- sskit/utils.py
+++ sskit/utils.py
@@ -27,27 +27,6 @@ imwrite(im[0,:,:], 't.jpg')
 import numpy as np
 import json
 
-camera_matrix = np.load(d / "camera_matrix.npy")
-
-def world_to_image(x, y, z):
-    with open(d / "lens.json") as fd:
-        lens = json.load(fd)
-    dist_poly = lens["dist_poly"]
-    sensor_width = lens["sensor_width"]
-    pixel_width = lens["pixel_width"]
-
-    _, height, width = im.shape
-    image_scale = sensor_width / width
-    principal = np.array([width / 2, height / 2])
-    x, y, z, _ = camera_matrix @ (x, y, z, 1)
-    cx = x / z
-    cy = y / z
-    rr = np.sqrt(cx ** 2 + cy ** 2)
-    arg = np.arctan2(cy, cx)
-    rr = a2d(np.arctan(rr), dist_poly) / pixel_width / sensor_width
-    cx = width * rr * np.cos(arg)
-    cy = width * rr * np.sin(arg)
-    return cx + principal[0], cy + principal[1]
 
 def a2d(a, dist_poly):
     a0 = -a * 180 / np.pi
@@ -57,6 +36,31 @@ def a2d(a, dist_poly):
             a0 - sum(k * x ** i for i, k in enumerate(dist_poly) if i != 1)
         ) / dist_poly[1]
     return x
+
+def world_to_image(x, y, z):
+    camera_matrix = np.load(d / "camera_matrix.npy")
+    with open(d / "lens.json") as fd:
+        lens = json.load(fd)
+    dist_poly = lens["dist_poly"]
+    sensor_width = lens["sensor_width"]
+    pixel_width = lens["pixel_width"]
+
+    x, y, z, _ = camera_matrix @ (x, y, z, 1)
+    cx = x / z
+    cy = y / z
+
+    rr = np.sqrt(cx ** 2 + cy ** 2)
+    # arg = np.arctan2(cy, cx)
+    rr2 = a2d(np.arctan(rr), dist_poly) / pixel_width / sensor_width
+    # cx =  rr2 * np.cos(arg)
+    # cy =  rr2 * np.sin(arg)
+
+    cx *= rr2/rr
+    cy *= rr2/rr
+
+    _, height, width = im.shape
+    principal = np.array([width / 2, height / 2])
+    return width * cx + principal[0], width * cy + principal[1]
 
 # ox, oy =
 world_to_image(30.5681917364984, 31.9043639410341, 0.0356437899172306)  # 2507.72959770331, 495.515473359975
