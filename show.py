e900664218c6f6ef3e903c90a4246b9417aaa744
Hakan Ardo <hakan.ardo@math.lth.se>
2024-08-21 09:19:51 +0200

    pelvis

--- show.py
+++ show.py
@@ -5,11 +5,12 @@ import torch
 
 d = Path("/home/hakan/data/SoccerCrowdV1/20240103_213931_node052_10173952_3348495_001_000/StudenternasCenterLeft/")
 camera_matrix, dist_poly = load_camera(str(d), 8)
+name = 'pelvis'
 
 img = imread(d / "rgb.jpg")
 objects = json.loads((d / "objects.json").read_bytes())
-pkt = torch.tensor([obj['keypoints'].get('left_foot') for obj in objects.values() if obj['class'] == 'human'])
-pkt_img = torch.tensor([obj['keypoints'].get('left_foot_img') for obj in objects.values() if obj['class'] == 'human'])
+pkt = torch.tensor([obj['keypoints'].get(name) for obj in objects.values() if obj['class'] == 'human'])
+pkt_img = torch.tensor([obj['keypoints'].get(name + '_img') for obj in objects.values() if obj['class'] == 'human'])
 
 npkt = world_to_image(camera_matrix[None], dist_poly[None], pkt)
 _, h, w = img.shape
